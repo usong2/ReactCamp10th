63cffee90bd0f22498045a945360abc78ce542a3
usong2 <dbthd6@naver.com>
2020-02-06 23:50:08 +0900

    withAuth

--- week2/react-router-example/src/App.js
+++ week2/react-router-example/src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Links from "./components/Links";
+import Login from "./pages/Login";
+
+import withAuth from "./hocs/withAuth";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/about" component={About} />
+      <Links />
+      <Switch>
+        <Route path="/profile/:id" exact component={withAuth(Profile)} />
+        <Route path="/profile" component={withAuth(Profile)} />
+        <Route path="/about" component={About} />
+        <Route path="/login" component={Login} />
+        <Route path="/" exact component={Home} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
